83040937dbb6b94400fe5617b85c7a5b60d3cabc
AnaPaulaMaximo <ap.maximo01@gmail.com>
2026-08-27 22:03:17 -0300

    primeiro commit

---
+++ api.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/',(req,res) => {
+    res.json({
+        date: new Date().toLocaleDateString('pt-BR'),
+        status: 'API Rodando'
+    })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () =>{
+    console.log("Servidor rodando...")
+})
